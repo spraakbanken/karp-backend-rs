065a9ec3c9f92c46c07645597f89f12bacb2854a
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-09-29 10:24:15 +0200

    feat: add accessor

--- libs/karp-lex/src/domain/entities/entry.rs
+++ libs/karp-lex/src/domain/entities/entry.rs
@@ -5,7 +5,14 @@ pub struct Entry<E> {
     entity_id: uuid::Uuid,
     last_modified_by: String,
     entry: E,
+    entry_id: String,
     discarded: bool,
     version: u32,
     last_modified: DateTime,
+}
+
+impl<E> Entry<E> {
+    pub fn entry(&self) -> &E {
+        &self.entry
+    }
 }
